28700b096ac79388c8888de14f5c24cb4cd4a108
karelibaug-glitch <karelibaug@gmail.com>
2026-09-16 20:31:37 +0530

    Add GitHub Actions APK Builder with USB UVC & WebRTC support

---
+++ build-www.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const targetDir = path.join(__dirname, 'www');
+
+if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+}
+
+const copyList = [
+    'index.html',
+    'index_v2.html',
+    'switcher.html',
+    'director_suite.html',
+    'guest.html',
+    'ai_audio.html',
+    'graphics.html',
+    'demo_scenes_and_audio.html',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png',
+    'sw.js'
+];
+
+copyList.forEach(file => {
+    const src = path.join(__dirname, file);
+    const dest = path.join(targetDir, file);
+    if (fs.existsSync(src)) {
+        fs.copyFileSync(src, dest);
+        console.log(`Copied ${file} -> www/${file}`);
+    }
+});
+
+function copyDirRecursive(src, dest) {
+    if (!fs.existsSync(src)) return;
+    if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+    const entries = fs.readdirSync(src, { withFileTypes: true });
+    for (let entry of entries) {
+        const srcPath = path.join(src, entry.name);
+        const destPath = path.join(dest, entry.name);
+        if (entry.isDirectory()) {
+            copyDirRecursive(srcPath, destPath);
+        } else {
+            fs.copyFileSync(srcPath, destPath);
+        }
+    }
+}
+
+copyDirRecursive(path.join(__dirname, 'js'), path.join(targetDir, 'js'));
+copyDirRecursive(path.join(__dirname, 'css'), path.join(targetDir, 'css'));
+
+console.log('✓ www directory successfully prepared!');
