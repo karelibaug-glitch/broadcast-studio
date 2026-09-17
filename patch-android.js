ebf9e507a68666991774bcd0b16ebd3dd189ab5c
karelibaug-glitch <karelibaug@gmail.com>
2026-09-17 11:17:32 +0530

    fix(android): change onDestroy visibility to public in MainActivity for Capacitor BridgeActivity

--- patch-android.js
+++ patch-android.js
@@ -429,7 +429,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         if (usbCameraBridge != null) {
             usbCameraBridge.stopCapture();
